20ddf0b0c5b73b6a98bc1deb512f49f360da6108
Leo Rainer <lirainer@lbl.gov>
2016-07-19 11:25:19 -0700

    fix f_roofCpTheta for roof pitch > 28

--- functions.cpp
+++ functions.cpp
@@ -2060,7 +2060,7 @@ void f_roofCpTheta(double* Cproof, int& windAngle, double* Cppitch, double& roof
 		if(roofPitch < 28)
 			F = C;
 		else
-			F = pow(abs(C),5) * C;	// maintain sign
+			F = pow(C,5);
 
 		Cppitch[i] = (Cproof[0] + Cproof[1]) * pow(C,2);
 		Cppitch[i] = Cppitch[i] + (Cproof[0] - Cproof[1]) * F;
